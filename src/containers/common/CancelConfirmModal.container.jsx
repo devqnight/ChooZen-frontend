2732ab781e9b04b47e0a70aa9aabce0bff3468da
sibzou <91130460+sibzou@users.noreply.github.com>
2022-04-24 23:35:33 +0200

    CancelConfirmModal: move the style sheet in the render function
    It is needed to include the theme background color in
    the style sheet.
    
    This is a cut-paste only commit. The code block is
    unmodified.

--- src/containers/common/CancelConfirmModal.container.jsx
+++ src/containers/common/CancelConfirmModal.container.jsx
@@ -2,6 +2,34 @@ import React from "react";
 import {View, Modal, Text, StyleSheet} from "react-native";
 
 const CancelConfirmModal = (props) => {
+    const styles = StyleSheet.create({
+        modalView: {
+            height: "100%",
+            padding: 10,
+            backgroundColor: "white"
+        },
+        content: {
+            flex: 1
+        },
+        bottomButtons: {
+            flexDirection: 'row',
+            justifyContent: "center",
+            marginTop: 10
+        },
+        bottomButton: {
+            borderRadius: 20,
+            backgroundColor: "orchid",
+            flex: 1,
+            padding: 10,
+            maxWidth: 200,
+            color: "white",
+            textAlign: "center"
+        },
+        bottomRightButton: {
+            marginStart: 10
+        }
+    })
+
     return (
         <Modal animationType="slide"
             transparent={false}
@@ -34,31 +62,3 @@ const CancelConfirmModal = (props) => {
 };
 
 export {CancelConfirmModal};
-
-const styles = StyleSheet.create({
-    modalView: {
-        height: "100%",
-        padding: 10,
-        backgroundColor: "white"
-    },
-    content: {
-        flex: 1
-    },
-    bottomButtons: {
-        flexDirection: 'row',
-        justifyContent: "center",
-        marginTop: 10
-    },
-    bottomButton: {
-        borderRadius: 20,
-        backgroundColor: "orchid",
-        flex: 1,
-        padding: 10,
-        maxWidth: 200,
-        color: "white",
-        textAlign: "center"
-    },
-    bottomRightButton: {
-        marginStart: 10
-    }
-})
