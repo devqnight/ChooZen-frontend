26dc23d73b1f1f47f14d39406e8152fe7ef358f1
kevinBoudina <kevin.boudina6@etu.univ-lorraine.fr>
2022-05-10 03:01:53 +0200

    test group reducer

--- src/reducers/_test_/group.reducer.test.jsx
+++ src/reducers/_test_/group.reducer.test.jsx
@@ -9,7 +9,7 @@ test("should return CHANGE_GROUP_FAILURE", () => {
   };
   const state = groups(undefined, action);
   expect(state).toEqual({
-    ...state
+    ...state,isLoading: false
   });
 });
 
@@ -25,7 +25,7 @@ test("should return CHANGE_GROUP_SUCCESS", () => {
   };
   const state = groups(undefined, action);
   expect(state).toEqual({
-    ...state
+    ...state,isLoading: false,
   })
 });
 
@@ -41,6 +41,200 @@ test("should return CHANGE_GROUP_REQUEST", () => {
   };
   const state = groups(undefined, action);
   expect(state).toEqual({
-    ...state
+    ...state,isLoading: true
   })
+});
+
+test("should return CHANGE_GROUP_FAILURE", () => {
+  const action = {
+    type: "CHANGE_GROUP_FAILURE",
+    payload: {
+      error: "error"
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  });
+});
+
+test("should return CREATE_GROUP_FAILURE", () => {
+  const action = {
+    type: "CREATE_GROUP_FAILURE",
+    payload: {
+      error: "error"
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  });
+});
+
+test("should return CREATE_GROUP_SUCCESS", () => {
+  const action = {
+    type: "CREATE_GROUP_SUCCESS",
+    payload: {
+      group: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  })
+});
+
+test("should return CREATE_GROUP_REQUEST", () => {
+  const action = {
+    type: "CREATE_GROUP_REQUEST",
+    payload: {
+      group: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: true
+  });
+});
+
+test("should return FETCH_GROUPS_FAILURE", () => {
+  const action = {
+    type: "FETCH_GROUPS_FAILURE",
+    payload: {
+      error: "error"
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  });
+});
+
+test("should return FETCH_GROUPS_SUCCESS", () => {
+  const action = {
+    type: "FETCH_GROUPS_SUCCESS",
+    payload: {
+      groups: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false,
+
+  })
+});
+
+test("should return FETCH_GROUPS_REQUEST", () => {
+  const action = {
+    type: "FETCH_GROUPS_REQUEST",
+    payload: {
+      groups: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: true
+  });
+});
+
+test("should return FETCH_GROUP_FAILURE", () => {
+  const action = {
+    type: "FETCH_GROUP_FAILURE",
+    payload: {
+      error: "error"
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  });
+});
+
+test("should return FETCH_GROUP_SUCCESS", () => {
+  const action = {
+    type: "FETCH_GROUP_SUCCESS",
+    payload: {
+      group: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  });
+});
+
+test("should return FETCH_GROUP_REQUEST", () => {
+  const action = {
+    type: "FETCH_GROUP_REQUEST",
+    payload: {
+      group: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: true
+  });
+});
+
+test("should return DELETE_GROUP_FAILURE", () => {
+  const action = {
+    type: "DELETE_GROUP_FAILURE",
+    payload: {
+      error: "error"
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  });
+});
+
+test("should return DELETE_GROUP_SUCCESS", () => {
+  const action = {
+    type: "DELETE_GROUP_SUCCESS",
+    payload: {
+      group: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: false
+  });
+});
+
+test("should return DELETE_GROUP_REQUEST", () => {
+  const action = {
+    type: "DELETE_GROUP_REQUEST",
+    payload: {
+      group: {
+        id: 1,
+        name: "group"
+      }
+    }
+  };
+  const state = groups(undefined, action);
+  expect(state).toEqual({
+    ...state,isLoading: true
+  });
 });
